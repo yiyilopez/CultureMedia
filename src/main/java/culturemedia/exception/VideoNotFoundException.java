052c53d687df49d649a6b291893d7c8b424d9aad
DuvanTP <duvan.tapias@udea.edu.co>
2024-04-17 17:31:02 -0500

    Adding exception structures 2

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -1,9 +1,13 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
+
 public class VideoNotFoundException extends cultureMediaException {
-    public VideoNotFoundException() {}
+    public VideoNotFoundException() {
+        super("can't find any video");
+    }
 
     public VideoNotFoundException(String title) {
-
+        super(MessageFormat.format("the video with the title {0} was not found", title));
     }
 }
